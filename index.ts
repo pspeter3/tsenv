714299dbab2070f1563fe07cac0a00d096d7b2be
Phips Peter <pspeter333@gmail.com>
2015-12-15 20:19:24 -0800

    Add __extends
    This provides an equivalent of TypeScript's built in extend function.
    The hope is that this code is equally performant but more readable.

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-declare var global: any;
+function __extends(child: any, parent: any): void {
+    if (parent === null) {
+        child.prototype = Object.create(parent);
+    } else {
+        for (var name in parent) {
+            if (parent.hasOwnProperty(name)) {
+                child[name] = parent[name];
+            }
+        }
+        function __() {
+            this.constructor = child;
+        }
+        __.prototype = parent.prototype;
+        child.prototype = new (<any>__)();
+    }
+}
 
-global.tsenv = true;
+namespace tsenv {
+    declare var global: any;
+    if ((<any>global) !== undefined) {
+        global.__extends = __extends;
+    }
+}
--- spec.ts
+++ spec.ts
@@ -1,5 +1,56 @@
+declare var global: any;
+
 describe("tsenv", () => {
-    it("should exist", () => {
-        expect(global.tsenv).toBeTruthy();
+    describe("__extends", () => {
+        it("should handle null", () => {
+            var Foo: any = (function(__super: any) {
+                global.__extends(Foo, __super);
+                function Foo() {
+                }
+                return Foo;
+            })(null);
+            var foo = new Foo();
+            expect(foo instanceof Foo).toBeTruthy();
+        });
+
+        it("should handle inheritance", () => {
+            var Bar: any = (function() {
+                function Bar() {
+                }
+                return Bar;
+            })();
+            var Foo: any = (function(__super: any) {
+                global.__extends(Foo, __super);
+                function Foo() {
+                }
+                return Foo;
+            })(Bar);
+            var foo = new Foo();
+            expect(foo instanceof Bar).toBeTruthy();
+            expect(foo instanceof Foo).toBeTruthy();
+        });
+
+        it("should add super methods", () => {
+            var Bar: any = (function() {
+                function Bar() {
+                }
+                Bar.prototype.bar = function() {
+                    return "bar";
+                };
+                return Bar;
+            })();
+            var Foo: any = (function(__super: any) {
+                global.__extends(Foo, __super);
+                function Foo() {
+                }
+                Foo.prototype.foo = function() {
+                    return "foo";
+                };
+                return Foo;
+            })(Bar);
+            var foo = new Foo();
+            expect(foo.bar()).toBe("bar");
+            expect(foo.foo()).toBe("foo");
+        });
     });
 });
